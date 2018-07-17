af331f10c984cd9c1dc1b0b1e1c1bb8d82d81c2f
wjddls <wjddls0119@gmail.com>
2018-07-17 13:15:18 +0900

    api 만들기 전 소스코드

---
+++ led/app/src/main/java/com/example/m2a/led/MainActivity.java
@@ -0,0 +1,405 @@
+package com.example.m2a.led;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private final static int DEVICES_DIALOG = 1;
+    private final static int ERROR_DIALOG = 2;
+
+    public static Context mContext;
+    public static AppCompatActivity activity;
+
+    TextView myLabel, mRecv;
+    EditText myTextbox;
+    static BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+
+    //음성
+    Intent intent;
+    SpeechRecognizer mRecognizer;
+    TextView textView;
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button sendButton = (Button)findViewById(R.id.send);
+        myLabel = (TextView)findViewById(R.id.label);
+        myTextbox = (EditText)findViewById(R.id.entry);
+        mRecv = (TextView)findViewById(R.id.recv);
+
+        mContext = this;
+        activity=this;
+
+//1.블루투스 사용 가능한지 검사합니다.
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            ErrorDialog("This device is not implement Bluetooth.");
+            return;
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            ErrorDialog("This device is disabled Bluetooth.");
+            return;
+        }
+        else
+//2. 페어링 되어 있는 블루투스 장치들의 목록을 보여줍니다.
+//3. 목록에서 블루투스 장치를 선택하면 선택한 디바이스를 인자로 하여 doConnect 함수가 호출됩니다.
+            DeviceDialog();
+
+
+//11. Send 버튼을 누르면 sendData함수가 호출됩니다.
+        sendButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                try
+                {
+                    sendData();
+                }
+                catch (IOException ex) { }
+            }
+
+        });
+
+        //음성
+       /* setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_RECORD_AUDIO
+                );
+            }
+        }*/
+
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(recognitionListener);
+
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        Button button = (Button) findViewById(R.id.button01);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRecognizer.startListening(intent);
+            }
+        });
+    }
+
+    static public Set<BluetoothDevice> getPairedDevices() {
+        return mBluetoothAdapter.getBondedDevices();
+    }
+
+    @Override
+    public void onBackPressed() {
+        doClose();
+        super.onBackPressed();
+    }
+
+
+    //13. 백버튼이 눌러지거나, ConnectTask에서 예외발생시
+//데이터 수신을 위한 스레드를 종료시키고 CloseTask를 실행하여 입출력 스트림을 닫고,
+//소켓을 닫아 통신을 종료합니다.
+    public void doClose() {
+        workerThread.interrupt();
+        new CloseTask().execute();
+    }
+
+
+
+    public void doConnect(BluetoothDevice device) {
+        mmDevice = device;
+
+//Standard SerialPortService ID
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+        try {
+// 4. 지정한 블루투스 장치에 대한 특정 UUID 서비스를 하기 위한 소켓을 생성합니다.
+// 여기선 시리얼 통신을 위한 UUID를 지정하고 있습니다.
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+// 5. 블루투스 장치 검색을 중단합니다.
+            mBluetoothAdapter.cancelDiscovery();
+// 6. ConnectTask를 시작합니다.
+            new ConnectTask().execute();
+        } catch (IOException e) {
+            Log.e("", e.toString(), e);
+            ErrorDialog("doConnect "+e.toString());
+        }
+    }
+
+    private class ConnectTask extends AsyncTask<Void, Void, Object> {
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Object doInBackground(Void... params) {
+            try {
+//7. 블루투스 장치로 연결을 시도합니다.
+                mmSocket.connect();
+
+//8. 소켓에 대한 입출력 스트림을 가져옵니다.
+                mmOutputStream = mmSocket.getOutputStream();
+                mmInputStream = mmSocket.getInputStream();
+
+//9. 데이터 수신을 대기하기 위한 스레드를 생성하여 입력스트림로부터의 데이터를 대기하다가
+// 들어오기 시작하면 버퍼에 저장합니다.
+// '\n' 문자가 들어오면 지금까지 버퍼에 저장한 데이터를 UI에 출력하기 위해 핸들러를 사용합니다.
+                beginListenForData();
+
+
+            } catch (Throwable t) {
+                Log.e( "", "connect? "+ t.getMessage() );
+                doClose();
+                return t;
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Object result) {
+//10. 블루투스 통신이 연결되었음을 화면에 출력합니다.
+            myLabel.setText("Bluetooth Opened");
+            if (result instanceof Throwable)
+            {
+                Log.d("","ConnectTask "+result.toString() );
+                ErrorDialog("ConnectTask "+result.toString());
+
+            }
+        }
+    }
+    private class CloseTask extends AsyncTask<Void, Void, Object> {
+        @Override
+        protected Object doInBackground(Void... params) {
+            try {
+                try{mmOutputStream.close();}catch(Throwable t){/*ignore*/}
+                try{mmInputStream.close();}catch(Throwable t){/*ignore*/}
+                mmSocket.close();
+            } catch (Throwable t) {
+                return t;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+            if (result instanceof Throwable) {
+                Log.e("",result.toString(),(Throwable)result);
+                ErrorDialog(result.toString());
+            }
+        }
+    }
+
+
+
+    public void DeviceDialog()
+    {
+        if (activity.isFinishing()) return;
+
+        FragmentManager fm = MainActivity.this.getSupportFragmentManager();
+        MyDialogFragment alertDialog = MyDialogFragment.newInstance(DEVICES_DIALOG, "");
+        alertDialog.show(fm, "");
+    }
+
+
+
+    public void ErrorDialog(String text)
+    {
+        if (activity.isFinishing()) return;
+
+        FragmentManager fm = MainActivity.this.getSupportFragmentManager();
+        MyDialogFragment alertDialog = MyDialogFragment.newInstance(ERROR_DIALOG, text);
+        alertDialog.show(fm, "");
+    }
+
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler(Looper.getMainLooper());
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == '\n')
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            mRecv.setText(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    //12. UI에 입력된 문자열이 있다면 출력 스트림에 기록하고
+//화면에 "Data Sent"를 출력해줍니다.
+    void sendData() throws IOException
+    {
+        String msg = myTextbox.getText().toString();
+        if ( msg.length() == 0 ) return;
+
+        msg += "\n";
+        Log.d(msg, msg);
+        mmOutputStream.write(msg.getBytes());
+        myLabel.setText("Data Sent");
+        myTextbox.setText(" ");
+    }
+    //음성
+    private RecognitionListener recognitionListener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+        }
+
+        @Override
+        public void onError(int i) {
+            textView.setText("너무 늦게 말하면 오류뜹니다");
+
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            String key = "";
+            key = SpeechRecognizer.RESULTS_RECOGNITION;
+            ArrayList<String> mResult = bundle.getStringArrayList(key);
+
+            Button sendButton = (Button)findViewById(R.id.send);
+
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+
+            textView.setText(rs[0]);
+            if(textView.getText().toString().equals("불 켜"))
+            {
+                myTextbox.setText("a");
+                textView.setText("a");
+
+            }
+
+            else if(textView.getText().toString().equals("불 꺼")){
+                myTextbox.setText("b");
+                textView.setText("b");
+            }
+            sendButton.performClick();
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+        }
+    };
+}
+
